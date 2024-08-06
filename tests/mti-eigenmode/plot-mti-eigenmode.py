fd0a13d459361b6a3fe10c8a7698cc0620cfdf2f
Lorenzo Maria Perrone <lorenzo.perrone.lmp@gmail.com>
2024-08-06 16:12:05 +0200

    python plotting script mti-eigenmode test

--- tests/mti-eigenmode/plot-mti-eigenmode.py
+++ tests/mti-eigenmode/plot-mti-eigenmode.py
@@ -5,7 +5,7 @@ import h5py
 import glob
 import os
 
-sp_savedir = "../../build/tests/alfven-wave/data/"
+sp_savedir = "../../build/tests/mti-eigenmode/data/"
 sp_savename = 'snap'
 
 if not os.path.exists(sp_savedir+'Plots'):
@@ -25,7 +25,14 @@ lx = 1.0
 ly = 1.0
 lz = 1.0
 
-nu = 1./1000.
+reynolds_m = 100000
+reynolds_ani = 100
+sigma  = 0.9341413811120219
+Pe     = reynolds_ani
+Reeta  = reynolds_m
+kparallel  = (2.0*np.pi/lx)*12.0
+B0 = 1e-4
+N2 = 0.1
 
 x = -0.5 + lx * (np.arange(nx)) / nx
 y = -0.5 + ly * (np.arange(ny)) / ny
@@ -33,16 +40,18 @@ z = -0.5 + lz * (np.arange(nz)) / nz
 
 X, Y, Z = np.meshgrid(x,y,z,indexing='ij')
 
-vx_0 =   1e-5*np.exp(1j*2.0*np.pi*Z/lz - 1j*np.pi/2.0)
-vy_0 =   np.zeros(X.shape)
-vz_0 =   np.zeros(X.shape)
+vx_0 = np.zeros(X.shape)
+vy_0 = np.zeros(X.shape)
+vz_0 = -0.00001*np.sin(kparallel*X)
 
-bx_0 =   -1e-5*np.exp(1j*2.0*np.pi*Z/lz - 1j*np.pi/2.0)
-by_0 =   np.zeros(X.shape)
-bz_0 =   np.ones(X.shape)
+bx_0 = B0*np.ones(X.shape)
+by_0 = np.zeros(X.shape)
+bz_0 = -0.00001*np.cos(kparallel*X)*B0*kparallel/(sigma+kparallel*kparallel/Reeta)
+
+th_0 = 1.0/(sigma + kparallel*kparallel/Pe)*(N2 - kparallel*kparallel/Pe/(sigma+kparallel*kparallel/Reeta) ) * vz_0
 
 tol = 1e-15
-omega_A = (2.0*np.pi/lz)
+
 
 # e.g. t = 5
 # vx_0[0:10,0,0]*np.exp(-2.0*nu*t*(2.0*np.pi)**2)
@@ -60,30 +69,40 @@ for i in range(len(sp_data_list)):
     by = np.reshape(data_sp['by'],(nx,ny,nz))
     bz = np.reshape(data_sp['bz'],(nx,ny,nz))
 
+    th = np.reshape(data_sp['th'],(nx,ny,nz))
+
     t =  data_sp['t_save'][0]
     data_sp.close()
 
-    vx_analytical = (vx_0 * np.exp(-1j*omega_A*t)).real
-    vy_analytical = (vy_0 * np.exp(-1j*omega_A*t)).real
-    vz_analytical = vz_0
+    vx_analytical = vx_0
+    vy_analytical = vy_0
+    vz_analytical = vz_0 * np.exp(sigma*t)
 
-    bx_analytical = (bx_0 * np.exp(-1j*omega_A*t)).real
-    by_analytical = (by_0 * np.exp(-1j*omega_A*t)).real
-    bz_analytical = bz_0
+    bx_analytical = bx_0
+    by_analytical = by_0
+    bz_analytical = bz_0 * np.exp(sigma*t)
 
-    fig, ax = plt.subplots(2,1)
+    th_analytical = th_0 * np.exp(sigma*t)
 
-    ax[0].plot(z, vy_analytical[0,0,:], ls='', marker='x', markerfacecolor='none', label='analytical')
-    ax[0].plot(z, vy[0,0,:], ls='', marker='s', markerfacecolor='none', label='spooky')
+    fig, ax = plt.subplots(3,1)
 
-    ax[0].set_title(r'$\delta v_y$')
+    ax[0].plot(x, vz_analytical[:,0,0], ls='', marker='x', markerfacecolor='none', label='analytical')
+    ax[0].plot(x, vz[:,0,0], ls='', marker='s', markerfacecolor='none', label='spooky')
+
+    ax[0].set_title(r'$\delta v_z$')
     ax[0].legend()
 
-    ax[1].plot(z, by_analytical[0,0,:], ls='', marker='x', markerfacecolor='none', label='analytical')
-    ax[1].plot(z, by[0,0,:], ls='', marker='s', markerfacecolor='none', label='spooky')
+    ax[1].plot(x, bz_analytical[:,0,0], ls='', marker='x', markerfacecolor='none', label='analytical')
+    ax[1].plot(x, bz[:,0,0], ls='', marker='s', markerfacecolor='none', label='spooky')
 
-    ax[1].set_title(r'$\delta B_y$')
+    ax[1].set_title(r'$\delta B_z$')
     ax[1].legend()
+
+    ax[2].plot(x, th_analytical[:,0,0], ls='', marker='x', markerfacecolor='none', label='analytical')
+    ax[2].plot(x, th[:,0,0], ls='', marker='s', markerfacecolor='none', label='spooky')
+
+    ax[2].set_title(r'$\theta$')
+    ax[2].legend()
 
     fig.tight_layout(pad=1.00, h_pad=None, w_pad=None, rect=None)
 
