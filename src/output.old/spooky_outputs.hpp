49f85cc62015a8bf0a215adb4c78ceb49036bbfa
LorenzoLMP <lorenzo.perrone.lmp@gmail.com>
2024-09-27 17:00:47 +0200

    reorganized input output files

--- src/output.old/spooky_outputs.hpp
+++
@@ -1,8 +0,0 @@
-#include "common.hpp"
-
-class SpookyOutput { // The class
-    public:
-        int length; // Number of spooky variables
-        char** name; // Names of variables (need to be allocated properly)
-        // scalar_type computeEnergy();
-};
