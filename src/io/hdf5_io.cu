6c5abe441e0fd356f27013f2087e3af729a5a8db
LorenzoLMP <lorenzo.perrone.lmp@gmail.com>
2024-10-09 21:18:03 +0200

    finished IO for restarting from files (hopefully

--- src/io/hdf5_io.cu
+++ src/io/hdf5_io.cu
@@ -1,4 +1,6 @@
 #include "define_types.hpp"
+#include <filesystem>
+// #include <stdlib.h>
 // #include "fields.hpp"
 #include "cufft_routines.hpp"
 #include "spooky.hpp"
@@ -13,6 +15,7 @@
 
 using namespace HighFive;
 
+int fileCounter(std::string dir);
 
 void InputOutput::WriteDataFile(Fields &fields, Parameters &param, TimeStepping &timestep) {
 
@@ -111,7 +114,7 @@ void InputOutput::ReadDataFile(Fields &fields, Parameters &param, TimeStepping &
 
     char data_snap_name[16];
     double time_save;
-
+    int restart_tmp;
     long int idx, idx_complex;
     unsigned int save_step;
     scalar_type *scratch;
@@ -126,80 +129,137 @@ void InputOutput::ReadDataFile(Fields &fields, Parameters &param, TimeStepping &
      * find the most recent data file
      * **************************/
 
-    if (restart_num == -1){
-        // find the most recent data file
-    }
-    else {
-        std::sprintf(data_snap_name,"snap%04i.h5",restart_num);
-    }
+    // find the most recent data file
+    int most_recent_snap = fileCounter(param.output_dir + std::string("/data/"));
+
+    if (most_recent_snap > 0) { // some data files exist!
+        if (restart_num > most_recent_snap){
+            std::printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+            std::printf("!!!! Error: restart file not found, restarting from last available data file...\n");
+            std::printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+            restart_tmp = most_recent_snap;
+        }
+        else if (restart_num == -1){
+            std::printf("Restarting from last available data file...\n");
+            restart_tmp = most_recent_snap;
+        }
+        else {
+            std::cout << "Data file selected for restarting: \t" << restart_num << std::endl;
+            restart_tmp = restart_num;
+        }
+        std::sprintf(data_snap_name,"snap%04i.h5",restart_tmp);
+
+        std::string fname = param.output_dir + std::string("/data/") + std::string(data_snap_name);
+
+        // std::printf("Reading from data file %s \n",fname);
+        std::cout << "Data file selected for restarting: \t" << fname << std::endl;
+
+        // sanity check
+        if (std::filesystem::exists(fname)){
+            std::cout << "The data file requested exists. Attempting to read data from it..." << std::endl;
+        }
+        else{
+            std::printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+            std::printf("!!!! Error: restart file not found, check that you are choosing the right file.\n");
+            std::printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+            // return;
+            exit(0);
+        }
+
+        try {
+            File file(fname, File::ReadOnly);
+
+            DataSet dataset = file.getDataSet("t_save");
+            dataset.read(time_save);
+
+            dataset = file.getDataSet("t_lastsnap");
+            dataset.read(t_lastsnap);
+
+            dataset = file.getDataSet("t_lastvar");
+            dataset.read(t_lastvar);
+
+            dataset = file.getDataSet("step");
+            dataset.read(save_step);
+
+            // std::printf("time_save: %.2f \t tend: %.2f \n",param.t_initial,timestep.current_time,param.t_final);
+            std::printf("t_lastsnap: %.2e \t t_lastvar: %.2e \n",t_lastsnap,t_lastvar);
+
+            num_save = restart_tmp+1;
+            timestep.current_time = time_save;
+            timestep.current_step = save_step;
 
 
-    std::string fname = param.output_dir + std::string("/data/") + std::string(data_snap_name);
-
-    // std::printf("Reading from data file %s \n",fname);
-    std::cout << "Reading from data file \t" << fname << std::endl;
-
-    try {
-        File file(fname, File::ReadOnly);
-
-        DataSet dataset = file.getDataSet("t_save");
-        dataset.read(time_save);
-
-        dataset = file.getDataSet("t_lastsnap");
-        dataset.read(t_lastsnap);
-
-        dataset = file.getDataSet("t_lastvar");
-        dataset.read(t_lastvar);
-
-        dataset = file.getDataSet("step");
-        dataset.read(save_step);
-
-        // std::printf("time_save: %.2f \t tend: %.2f \n",param.t_initial,timestep.current_time,param.t_final);
-        std::printf("t_lastsnap: %.2e \t t_lastvar: %.2e \n",t_lastsnap,t_lastvar);
-
-        num_save = restart_num+1;
-        timestep.current_time = time_save;
-        timestep.current_step = save_step;
+            std::printf("-----------------Reading from snap %04i at time_save: %.2e \n",restart_tmp,time_save);
 
 
-        std::printf("-----------------Reading from snap %04i at time_save: %.2e \n",restart_num,time_save);
+            // Now read data_fields
+            for (int n = 0; n < fields.num_fields; n++){
 
+                std::string var_name(list_of_variables[n]);
+                dataset = file.getDataSet(var_name);
+                //
+                // let's read our vector into the scratch
+                dataset.read(scratch);
 
-        // Now read data_fields
-        for (int n = 0; n < fields.num_fields; n++){
+                // let's rearrange it to remove zero padding
+                for (int i = 0; i < nx; i++){
+                    for (int j = 0; j < ny; j++){
+                        for (int k = 0; k < nz; k++){
+                            // two indices
+                            idx = k + nz * ( j + i * ny);
+                            idx_complex = k + (nz/2+1)*2 * ( j + i * ny);
 
-            std::string var_name(list_of_variables[n]);
-            dataset = file.getDataSet(var_name);
-            //
-            // let's read our vector into the scratch
-            dataset.read(scratch);
-
-            // let's rearrange it to remove zero padding
-            for (int i = 0; i < nx; i++){
-                for (int j = 0; j < ny; j++){
-                    for (int k = 0; k < nz; k++){
-                        // two indices
-                        idx = k + nz * ( j + i * ny);
-                        idx_complex = k + (nz/2+1)*2 * ( j + i * ny);
-
-                        fields.farray_r[n][idx_complex] = scratch[idx];
-                        // if (i < 10 && k == 0) std::printf("farray_r %.2e \n",scratch[idx]);
+                            fields.farray_r[n][idx_complex] = scratch[idx];
+                            // if (i < 10 && k == 0) std::printf("farray_r %.2e \n",scratch[idx]);
+                        }
                     }
                 }
             }
-        }
-        std::printf("Finished reading data file.\n");
+            std::printf("Finished reading data file.\n");
 
-    } catch (const Exception& err) {
-        // std::cerr << err.what() << std::endl;
-        // std::exit(1);
+        } catch (const Exception& err) {
+            // std::cerr << err.what() << std::endl;
+            // std::exit(1);
+            std::printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+            std::printf("!!!! Error: could not read from data file. \n");
+            std::printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
+            // param.restart = 0;
+            exit(0);
+
+        }
+
+    }
+    else { // no datafiles found
         std::printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
-        std::printf("!!!! Error: restart file not found, starting a new run from t=0.00.\n");
+        std::printf("!!!! Error: no restart files found, starting a new run...\n");
         std::printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
         param.restart = 0;
-
     }
 
     free(scratch);
 
+}
+
+// from https://stackoverflow.com/a/36878471
+int fileCounter(std::string dir){
+    int returnedCount = -1;
+    int possibleMax = 5000000; //some number you can expect.
+    bool status;
+    char data_snap_name[16];
+
+    for (int istarter = 0; istarter < possibleMax; istarter++){
+        std::string fileName = "";
+        fileName.append(dir);
+        // fileName.append(prefix);
+        std::sprintf(data_snap_name,"snap%04i.h5",istarter);
+        // fileName.append(to_string(istarter));
+        fileName.append(data_snap_name);
+        status = std::filesystem::exists(fileName);
+
+        if (!status)
+            break;
+
+        returnedCount++;
+    }
+    return returnedCount;
 }
